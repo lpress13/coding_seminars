0e78205c1bd6415ced1d1650f5f85553e4a8ab07
Mason F. Matthews <mason.f.matthews@gmail.com>
2017-02-09 13:51:09 -0500

    Improve in-class example

--- intro_to_coding/5/in_class/user_file.rb
+++ intro_to_coding/5/in_class/user_file.rb
@@ -3,25 +3,37 @@ require 'csv'
 class UserFile
   def initialize(filename)
     @rows = []
+    row_number = 0
     CSV.foreach(filename, headers: true) do |row|
-      @rows = UserRow.new(row)
+      @rows << UserRow.new(row, row_number)
+      row_number += 1
     end
   end
 
-  def invalid_rows
-    @rows.reject {|r| r.valid?}
+  def invalid_row_numbers
+    @rows.reject {|r| r.valid?}.map {|r| r.number}
   end
 end
 
 class UserRow
-  SPECIAL_CHARACTERS = "!#$%&'*+-/=?^_`{|}~"
+  attr_reader :number
 
-  def initialize(row)
+  def initialize(row, number)
     @row = row
+    @number = number
   end
 
   def valid_email?
-    @row[:email].match(/^((\w+\.{0,1}\w+)+|([\w#{SPECIAL_CHARACTERS}]+))@\w([\-\w]|(\w\.\w))*\.\w+$/)
+    special = "!#$%&'*+-/=?^_`{|}~"
+    @row[:email].match(/^((\w+\.{0,1}\w+)+|([\w#{special}]+))@\w([\-\w]|(\w\.\w))*\.\w+$/)
+  end
+
+  def valid_phone?
+    @row[:phone].match(/^d{3}-d{3}-d{4}$/)
+  end
+
+  def valid_date?
+    @row[:joined].match(/^d{2}\/d{2}\/d{4}$/)
   end
 
   def valid?
